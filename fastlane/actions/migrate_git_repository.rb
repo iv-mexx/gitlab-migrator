a5fdd7edf53c0fb7f2ab1c721044f8aad3cbde4f
Marc Sutter <marc.sutter@swisscom.com>
2016-09-06 16:36:25 +0200

    skip migration error by empty repositories (also if wiki is empty)

--- fastlane/actions/migrate_git_repository.rb
+++ fastlane/actions/migrate_git_repository.rb
@@ -8,14 +8,17 @@ module Fastlane
       def self.run(params)
         Dir.mktmpdir do |tmpdir|
           Helper.log.info "Cloning Repository from #{params[:from_repo_url]} into #{tmpdir}"
-          Actions.sh("git clone #{params[:from_repo_url]} #{tmpdir}")
-          Dir.chdir(tmpdir) do
-            Actions.sh("git remote add newOrigin #{params[:to_repo_url]}")
-            default_branch = Actions.sh("git rev-parse --abbrev-ref HEAD")
-            Actions.sh("for remote in `git branch -r | grep -v #{default_branch} `; do git checkout --track $remote ; done")
-            Helper.log.info "Pushing all branches and tags to #{params[:to_repo_url]}"          
-            Actions.sh("git push newOrigin --all")
-            Actions.sh("git push newOrigin --tags")
+          clone_result = Actions.sh("git clone #{params[:from_repo_url]} #{tmpdir}")
+          if clone_result.include?("warning: You appear to have cloned an empty repository.")
+              Helper.log.info "Skipping empty repository #{params[:to_repo_url]}"
+          else
+            Dir.chdir(tmpdir) do
+              Actions.sh("git remote add newOrigin #{params[:to_repo_url]}")
+              default_branch = Actions.sh("git rev-parse --abbrev-ref HEAD")
+              Actions.sh("for remote in `git branch -r | grep -v #{default_branch} `; do git checkout --track $remote ; done")
+              Actions.sh("git push newOrigin --all")
+              Actions.sh("git push newOrigin --tags")
+            end
           end
         end
       end
@@ -36,13 +39,13 @@ module Fastlane
         # Define all options your action supports.
         [
           FastlaneCore::ConfigItem.new(key: :from_repo_url,
-           env_name: "FL_MIGRATE_GIT_REPOSITORY_FROM_REPO_URL", 
+           env_name: "FL_MIGRATE_GIT_REPOSITORY_FROM_REPO_URL",
            description: "The url of the source remote from which the repository should be cloned",
            verify_block: proc do |value|
             raise "No Source URL for MigrateGitRepositoryAction given, pass using `from_repo_url: 'url'`".red unless (value and not value.empty?)
           end),
           FastlaneCore::ConfigItem.new(key: :to_repo_url,
-           env_name: "FL_MIGRATE_GIT_REPOSITORY_TO_REPO_URL", 
+           env_name: "FL_MIGRATE_GIT_REPOSITORY_TO_REPO_URL",
            description: "The url of the target remote to which the repository should be migrated",
            verify_block: proc do |value|
             raise "No Destination URL for MigrateGitRepositoryAction given, pass using `to_repo_url: 'url'`".red unless (value and not value.empty?)
